58bf5732752a0ac5bd38e9f0417de5a882b4e7fc
Jakub Schwark <jakubschwark@gmail.com>
2022-03-21 21:12:15 +0100

    first

---
+++ main.js
@@ -0,0 +1,19 @@
+const myBtn = document.getElementById('add');
+const list = document.getElementById('list');
+
+const addToList = () => {
+    let txtField = document.getElementById('text-field-input').value;
+    console.log(txtField);
+    let entryDiv = document.createElement('div');
+    let entryP = document.createElement('p');
+    entryP.appendChild(document.createTextNode(txtField));
+    entryDiv.appendChild(entryP);
+    let entryButton = document.createElement('button')
+    entryButton.appendChild(document.createTextNode('-'));
+    entryDiv.appendChild(entryButton);
+    list.appendChild(entryDiv);
+    document.getElementById('text-field-input').value = ''
+    entryButton.addEventListener('click', function() {entryDiv.remove()})
+}
+
+myBtn.addEventListener('click', addToList);
